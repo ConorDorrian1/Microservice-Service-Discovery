b15f26a2de6d2042ee2b4091d72cd1ec98bb4ac2
ConorDorrian1 <L00101441@student.lyit.ie>
2019-12-11 19:18:38 +0000

    adding configuration to application.yml

--- src/main/java/com/masters/eureka/discoveryservice/DiscoveryServiceApplication.java
+++ src/main/java/com/masters/eureka/discoveryservice/DiscoveryServiceApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 @EnableEurekaServer //Lets spring boot know that it is a eureka server  http://localhost:8761/
 public class DiscoveryServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
